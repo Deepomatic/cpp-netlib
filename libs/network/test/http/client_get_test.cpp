e5e613d948e458ff2b3de9e284b1b92fe5be634e
Hugo <hugo@deepomatic.com>
2017-10-19 14:32:35 +0200

    Ability to set a sni_hostname at the request level

--- libs/network/test/http/client_get_test.cpp
+++ libs/network/test/http/client_get_test.cpp
@@ -65,3 +65,45 @@ TYPED_TEST(HTTPClientTest, TemporaryClientObjectTest) {
   EXPECT_TRUE(response.status() == 200u ||
               (response.status() >= 300 && response.status() < 400));
 }
+
+
+typedef boost::network::http::basic_client<boost::network::http::tags::http_async_8bit_tcp_resolve, 1, 1> async_client;
+#define EXC_PTR(cmd) try { cmd } catch (const std::exception_ptr& ex) { std::rethrow_exception(ex); }
+
+TYPED_TEST(HTTPClientTest, ReuseResponse) {
+
+  async_client client;
+
+  async_client::request req("https://static.deepomatic.com/compass/NVR_ch2_J_20161209122514_20161209122514_orig.jpg");
+
+  req.sni_hostname(req.host());
+
+  auto response = client.get(req);
+
+  while (!ready(response));
+
+  EXC_PTR(response.status(););
+
+  // we get the expected headers and body
+  auto hdrs = headers(response);
+
+  for (auto &h : hdrs) {
+      std::cout << h.first << ": " << h.second << std::endl;
+  }
+
+  std::cout << "---" << std::endl;
+
+  req.uri("http://example.com"); // changing the url
+
+  response = client.get(req);
+
+  while (!ready(response));
+
+  // here the headers are the same as the previous response, they should be those from example.com
+  auto hdrs2 = headers(response);
+
+  for (auto &h : hdrs2) {
+      std::cout << h.first << ": " << h.second << std::endl;
+  }
+
+}
